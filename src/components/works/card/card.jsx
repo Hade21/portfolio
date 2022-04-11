4205d7367f8e8ca8c6f8a535ef744b0fa6f399cf
Muhammad Abdurrohman <abdurrohmanmuhammad123@gmail.com>
2022-04-11 21:54:01 +0700

    creating my-works page

--- src/components/works/card/card.jsx
+++ src/components/works/card/card.jsx
@@ -2,14 +2,33 @@ import React from "react";
 
 const Card = (props) => {
   return (
-    <div className="card">
-      <img src="" alt="" />
-      <div className="title">
-        <h1>{props.title}</h1>
-        <p>{props.desc}</p>
-        <div className="button">
-          <a href={props.url}>Live URL</a>
-          <a href={props.repo}>Repository</a>
+    <div className="card w-[220px] h-full">
+      <img
+        src={require(`../../../assets/${props.image}`)}
+        alt=""
+        className="w-[220px] h-[100px] object-cover"
+      />
+      <div className="title text-white text-left mt-2 flex flex-col h-[190px] justify-between">
+        <div className="text grid gap-1">
+          <h1 className="font-inter font-extrabold text-lg">{props.title}</h1>
+          <p className="font-nunito font-normal text-sm leading-[18px]">
+            {props.desc}
+          </p>
+        </div>
+        <div className="button flex flex-col gap-2 justify-between font-nunito text-sm">
+          <a
+            href={props.url}
+            className="px-3 py-1 border-2 border-pink w-full
+          "
+          >
+            Live URL
+          </a>
+          <a
+            href={props.repo}
+            className="px-3 py-1 border-2 border-myblue w-full"
+          >
+            Repository
+          </a>
         </div>
       </div>
     </div>
--- src/components/works/my-works.jsx
+++ src/components/works/my-works.jsx
@@ -1,15 +1,49 @@
 import React from "react";
 import Card from "./card/card";
+import { Link } from "react-router-dom";
 import { items } from "./card/works.js";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 export default function MyWorks() {
   return (
-    <div className="my-works">
+    <div className="my-works flex items-center w-screen h-screen">
       <div className="glassmorphism lg:w-[80%] mx-auto my-auto bg-black-glass shadow-glass backdrop-blur-glass rounded-[10px]">
-        <div className="works">
-          <h1>Here is ...</h1>
-          <p>some of My works</p>
-          <div className="card-wrapper">{items.map}</div>
+        <div className="wrapper px-16 py-8 flex flex-col gap-8 h-full">
+          <div className="works font-inter font-extrabold text-white text-left">
+            <h1 className="text-5xl">Here is ...</h1>
+            <p className="text-xl leading-5">some of My works</p>
+          </div>
+          <div className="wrapper h-2/3 w-full">
+            <Swiper
+              spaceBetween={50}
+              slidesPerView={3.6}
+              onSlideChange={() => console.log("slide change")}
+              onSwiper={(swiper) => console.log(swiper)}
+            >
+              {items.map((item) => {
+                return (
+                  <SwiperSlide>
+                    <Card
+                      image={item.image}
+                      title={item.title}
+                      desc={item.desc}
+                      url={item.url}
+                      repo={item.repo}
+                    />
+                  </SwiperSlide>
+                );
+              })}
+              ...
+            </Swiper>
+          </div>
+          <div className="button">
+            <Link to="/contact">
+              <button className="text-base font-inter font-extrabold px-4 py-2 border-2 border-white w-fit self-center md:text-4xl md:px-6 md:py-3 lg:text-lg cursor-pointer text-white">
+                Contact Me
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
